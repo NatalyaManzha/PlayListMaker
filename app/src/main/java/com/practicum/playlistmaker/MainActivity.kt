586d1be3cd70236f75ed4a47fb7bf57aa5199b9a
Natalya Manzha <natalia@manzha.ru>
2023-10-05 11:42:54 +0400

    Кнопки с Toast

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonSearch = findViewById<Button>(R.id.search)
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Жмякс!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonClickListener)
+
+        val buttonMediaLibrary = findViewById<Button>(R.id.media_library)
+        buttonMediaLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Упс! И снова ничего", Toast.LENGTH_SHORT).show()
+        }
+        val buttonSetting = findViewById<Button>(R.id.settings)
+        buttonSetting.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Наташа! Мы фсе сломали!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
