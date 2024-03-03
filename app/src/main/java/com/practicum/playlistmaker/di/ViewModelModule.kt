c644b9d93665562a717d8ec03a40c1a5e6ef54a6
Natalya Manzha <natalia@manzha.ru>
2024-03-03 14:01:28 +0400

    Fragments (ДЗ спринт 18)

--- app/src/main/java/com/practicum/playlistmaker/di/ViewModelModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/di/ViewModelModule.kt
@@ -1,5 +1,7 @@
 package com.practicum.playlistmaker.di
 
+import com.practicum.playlistmaker.medialibrary.ui.FavoritesViewModel
+import com.practicum.playlistmaker.medialibrary.ui.PlaylistsViewModel
 import com.practicum.playlistmaker.player.ui.PlayerViewModel
 import com.practicum.playlistmaker.search.ui.SearchViewModel
 import com.practicum.playlistmaker.settings.ui.SettingsViewModel
@@ -18,5 +20,13 @@ val viewModelModule = module {
 
     viewModel {
         PlayerViewModel(get(), get(), get(), get())
+    }
+
+    viewModel {
+        FavoritesViewModel()
+    }
+
+    viewModel {
+        PlaylistsViewModel()
     }
 }
---
+++ app/src/main/java/com/practicum/playlistmaker/medialibrary/ui/PlaylistsViewModel.kt
@@ -0,0 +1,16 @@
+package com.practicum.playlistmaker.medialibrary.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.practicum.playlistmaker.medialibrary.ui.models.FragmentUiState
+
+class PlaylistsViewModel : ViewModel() {
+    private val stateLiveData = MutableLiveData<FragmentUiState>()
+
+    init {
+        stateLiveData.value = FragmentUiState.Default
+    }
+
+    fun observeState(): LiveData<FragmentUiState> = stateLiveData
+}
